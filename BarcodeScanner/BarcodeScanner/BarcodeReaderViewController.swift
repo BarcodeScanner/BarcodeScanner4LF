f184bb716ba81672684f8fb66b2038ad2ec220ba
Leca Cristian <cristi.leca2000@yahoo.com>
2023-04-07 11:57:49 +0300

    Checking if the barcode is duplicated

--- BarcodeScanner/BarcodeScanner/BarcodeReaderViewController.swift
+++ BarcodeScanner/BarcodeScanner/BarcodeReaderViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SQFeedbackGenerator
 import AVFoundation
 
 class BarcodeReaderViewController: CameraBarcodeReaderViewController {
@@ -27,6 +28,7 @@ class BarcodeReaderViewController: CameraBarcodeReaderViewController {
         
         // self.focusMarkLayer.strokeColor = UIColor.yellow.cgColor
         self.torchButton.isHidden = !self.hasTorch()
+        self.updateNrOfScannedBarcodesLabel()
                 
         self.barcodeHandler = { barcodeValue in
             
@@ -38,7 +40,46 @@ class BarcodeReaderViewController: CameraBarcodeReaderViewController {
                     }
                 }
             }
-            
         }
+    }
+    func handle(_ barcodeValue: String) {
+        if !self.scannedEntries.contains(barcodeValue) {
+            self.hasScannedNewBarcodeWithValue(barcodeValue)
+        } else if self.lastScannedEntry != barcodeValue {
+            self.hasScannedDuplicateBarcodeWithValue(barcodeValue)
+        }
+    }
+    
+    // MARK: - Instance methods -
+    
+    func hasScannedNewBarcodeWithValue(_ barcodeValue: String) {
+        self.scannedEntries.append(barcodeValue)
+        self.lastScannedEntry = barcodeValue
+        
+        DispatchQueue.main.async {
+            SQFeedbackGenerator().generateFeedback(type: .notification)
+            self.updateNrOfScannedBarcodesLabel()
+        }
+    }
+    
+    func hasScannedDuplicateBarcodeWithValue(_ barcodeValue: String) {
+        self.lastScannedEntry = barcodeValue
+        
+        DispatchQueue.main.async {
+            SQFeedbackGenerator().generateFeedback(type: .error)
+        }
+    }
+    func updateNrOfScannedBarcodesLabel() {
+        
+        let nrOfEntries = self.scannedEntries.count
+        var nrOfScannedBarcodesText = ""
+        
+        if nrOfEntries == 1 {
+            nrOfScannedBarcodesText = NSLocalizedString("dvbarcodereaderviewcontroller.one_barcode_scanned", tableName: nil, bundle: Bundle.main, value: "1 Barcode Scanned", comment: "1 Barcode Scanned")
+        } else {
+            let strFormat = NSLocalizedString("dvbarcodereaderviewcontroller.multiple_barcodes_scanned_format", tableName: nil, bundle: Bundle.main, value: "%d Barcodes Scanned", comment: "{nr_of_barcodes} Barcodes Scanned")
+            nrOfScannedBarcodesText = String.localizedStringWithFormat(strFormat, self.scannedEntries.count)
+        }
+        self.numberOfScannedBarcodesLabel.text = nrOfScannedBarcodesText
     }
 }
