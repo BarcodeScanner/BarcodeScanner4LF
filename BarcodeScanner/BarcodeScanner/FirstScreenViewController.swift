6685fab206400d0044a1cb900edebee30b7f4794
Chisulescu Mihai <mihai.george.ch@gmail.com>
2023-04-07 12:56:15 +0300

    Included Realm Library

--- BarcodeScanner/BarcodeScanner/FirstScreenViewController.swift
+++ BarcodeScanner/BarcodeScanner/FirstScreenViewController.swift
@@ -3,14 +3,15 @@ import Combine
 
 class FirstScreenViewController: UIViewController {
     private var cancellables = Set<AnyCancellable>()
-
+    
     @IBOutlet weak var scanBarcode: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func didTouchScanBarcode(_ sender: UIButton) {
         if let readerVC = BarcodeReaderViewController.getBarcodeReaderViewController() {
             readerVC.closeAfterFirstRead = true
+        }
     }
 }
