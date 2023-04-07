7816360603e81ca042bd030f8b3b9f52e9f3c9e0
Ciobotaru Crina <crina_ciobotaru@yahoo.com>
2023-04-07 13:19:47 +0300

    Searching for barcode read in database

--- BarcodeScanner/BarcodeScanner/FirstScreenViewController.swift
+++ BarcodeScanner/BarcodeScanner/FirstScreenViewController.swift
@@ -15,4 +15,27 @@ class FirstScreenViewController: UIViewController {
             readerVC.closeAfterFirstRead = true
         }
     }
+    
+    func didReadBarcodeWithValue(_ stringValue: String?) {
+        guard let stringValue = stringValue else { return }
+        do {
+            
+            let realm = try Realm()
+            let products = realm.objects(Product.self)
+            let productsByBarcode = products.where { $0.barcode == stringValue }
+            if productsByBarcode.isEmpty {
+                let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                guard let manageProductViewController = storyboard.instantiateViewController(withIdentifier: "ManageProductViewController") as? ManageProductViewController else { return }
+                    manageProductViewController.productModel = Product(name: "", barcode: stringValue, quantity: 0)
+                    self.navigationController?.pushViewController(manageProductViewController, animated: true)
+                
+            } else {
+                guard let product = productsByBarcode.first, let manageProductViewController = self.storyboard?.instantiateViewController(withIdentifier: "ManageProductViewController") as? ManageProductViewController else { return }
+                manageProductViewController.productModel = product
+                self.navigationController?.pushViewController(manageProductViewController, animated: true)
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
