12270ec00956138a7192725bbad1f803fd892801
Ciobotaru Crina <crina_ciobotaru@yahoo.com>
2023-04-20 20:33:15 +0300

    Code reorganization

--- BarcodeScanner/BarcodeScanner/ApplicationManager.swift
+++ BarcodeScanner/BarcodeScanner/ApplicationManager.swift
@@ -6,3 +6,89 @@
 //
 
 import Foundation
+import RealmSwift
+
+class ApplicationManager {
+    static var shared = ApplicationManager()
+    var realmConfiguration: Realm.Configuration?
+    var user: User?
+    var realm: Realm?
+    @ObservedResults(Product.self) var products
+    
+    private init() {
+        app.syncManager.errorHandler = { syncError, session in
+            if let thisError = syncError as? SyncError {
+                switch thisError.code {
+                    
+                case .clientSessionError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .clientUserError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .clientInternalError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .clientResetError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .underlyingAuthError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .permissionDeniedError:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .invalidFlexibleSyncSubscriptions:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                case .writeRejected:
+                    if let errorInfo = thisError.compensatingWriteInfo {
+                        for anError in errorInfo {
+                            print(anError.reason as Any)
+                        }
+                    }
+                @unknown default:
+                    break
+                }
+            }
+        }
+    }
+    
+    func openFlexibleSyncRealm(for user: User) async throws -> Realm {
+        var config = user.flexibleSyncConfiguration()
+        config.objectTypes = [Product.self, Inventory.self]
+        let realm = try await Realm(configuration: config)
+        print("Successfully opened realm: \(realm)")
+        let subscriptions = realm.subscriptions
+        try await subscriptions.update {
+            if subscriptions.first(named: "all_products") == nil {
+                subscriptions.append(QuerySubscription<Product>(name: "all_products"))
+            }
+            if subscriptions.first(named: "all_inventories") == nil {
+                subscriptions.append(QuerySubscription<Inventory>(name: "all_inventories"))
+            }
+        }
+        return realm
+    }
+}
--- BarcodeScanner/BarcodeScanner/CreateInventoryViewController.swift
+++ BarcodeScanner/BarcodeScanner/CreateInventoryViewController.swift
@@ -5,27 +5,26 @@ class CreateInventoryViewController: UIViewController {
     @IBOutlet weak var inventoryName: UITextField!
     
     @IBAction func didTouchCreate(_ sender: UIButton) {
-        
+        guard let name = self.inventoryName.text,
+              let realm = ApplicationManager.shared.realm else {
+            // TODO: add message handling for case name is missing
+            return
+        }
+        let inventory = Inventory(name: name)
+        do {
+            try realm.write {
+                realm.add(inventory)
+                self.navigationController?.popViewController(animated: true)
+            }
+            // TODO: add message for succes
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+
     }
     
     @IBAction func didTouchCancel(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
