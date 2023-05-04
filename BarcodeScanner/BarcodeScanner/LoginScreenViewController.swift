4715fedd2dfabc4038a96dc7eb6b89b967192dc8
Chisulescu Mihai <mihai.george.ch@gmail.com>
2023-05-04 19:19:01 +0300

    Added validation for email and password

--- BarcodeScanner/BarcodeScanner/LoginScreenViewController.swift
+++ BarcodeScanner/BarcodeScanner/LoginScreenViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 import RealmSwift
 
 class LoginScreenViewController: UIViewController {
+    
+    let minPasswordLength = 6
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -21,19 +23,7 @@ class LoginScreenViewController: UIViewController {
         }
     }
     @IBAction func didTouchCreateAccount(_ sender: UIButton) {
-        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
-        Task.init {
-            do {
-                try await app.emailPasswordAuth.registerUser(email: email, password: password)
-                print("Successfully registered user")
-                let user = try await app.login(credentials: Credentials.emailPassword(email: email, password: password))
-                ApplicationManager.shared.user = user
-                self.continueApp(with: user)
-            } catch {
-                print("Failed to register user: \(error.localizedDescription)")
-                self.errorCreateAccount()
-            }
-        }
+        self.createAccount()
     }
     
     
@@ -47,6 +37,82 @@ class LoginScreenViewController: UIViewController {
             ApplicationManager.shared.user = user
             self.continueApp(with: user)
         }
+    }
+    
+    func createAccount() {
+        guard let emailAddress = self.emailTextField.text, let password = self.passwordTextField.text else { return }
+        var errorMessage = ""
+
+        if emailAddress.isEmpty {
+            errorMessage.append("Please enter your account email address.")
+        } else if self.validateEmail(enteredEmail: emailAddress) == false {
+            errorMessage.append("Please enter a valid email address.")
+        }
+
+        let inputPasswordLength = password.count
+
+        if password.isEmpty {
+            errorMessage.append("\n")
+            errorMessage.append("Please enter a password.")
+        } else if self.isValidPassword(enteredPassword: password) == false {
+            if(!NSPredicate(format:"SELF MATCHES %@", ".*[A-Z]+.*").evaluate(with: password)){
+                errorMessage.append("\n")
+                errorMessage.append("Your password must have at least one uppercase")
+            }
+            
+            if(!NSPredicate(format:"SELF MATCHES %@", ".*[0-9]+.*").evaluate(with: password)){
+                errorMessage.append("\n")
+                errorMessage.append("Your password must have at least one digit")
+            }
+            
+            if(!NSPredicate(format:"SELF MATCHES %@", ".*[!&^%$#@()/]+.*").evaluate(with: password)){
+                errorMessage.append("\n")
+                errorMessage.append("Your password must have at least one symbol")
+            }
+            
+            if(!NSPredicate(format:"SELF MATCHES %@", ".*[a-z]+.*").evaluate(with: password)){
+                errorMessage.append("\n")
+                errorMessage.append("Your password must have at least one lowercase.")
+            }
+            
+            if inputPasswordLength < minPasswordLength {
+                errorMessage.append("\n")
+                errorMessage.append("Your password must have 8 characters or more.")
+            }
+        }
+        
+        if !errorMessage.isEmpty {
+            self.errorCreateAccount(errorMessage: errorMessage)
+            return
+        }
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+        Task.init {
+            do {
+                try await app.emailPasswordAuth.registerUser(email: email, password: password)
+                print("Successfully registered user")
+                let user = try await app.login(credentials: Credentials.emailPassword(email: email, password: password))
+                ApplicationManager.shared.user = user
+                UserDefaults.standard.set(email, forKey: "useremail")
+                self.continueApp(with: user)
+            } catch {
+                print("Failed to register user: \(error.localizedDescription)")
+                self.errorCreateAccount()
+            }
+        }
+
+    }
+    public func isValidPassword(enteredPassword: String) -> Bool {
+        let passwordRegx = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&<>*~:`-]).{8,}$"
+        let passwordCheck = NSPredicate(format: "SELF MATCHES %@",passwordRegx)
+        return passwordCheck.evaluate(with: enteredPassword)
+    }
+    
+    func validateEmail(enteredEmail:String) -> Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: enteredEmail)
+
     }
     
     func continueApp(with user: User) {
@@ -72,8 +138,14 @@ class LoginScreenViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    func errorCreateAccount(errorMessage: String) {
+        let alert = UIAlertController(title: "Error", message: errorMessage, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func errorCreateAccount() {
-        let alert = UIAlertController(title: "Error", message: "Somethings is wrong", preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "Error", message: "Failed to register user", preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
